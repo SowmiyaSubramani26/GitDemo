ade0beb3ee64fd63d67e9c55c716b2b6c07ed790
SowmiyaSubramani26 <s.sowmiya2610@gmail.com>
2022-06-15 15:44:52 +0530

    Adding comments in Feedback page

--- cypress/integration/e2e/FeedbackForm.spec.js
+++ cypress/integration/e2e/FeedbackForm.spec.js
@@ -1,3 +1,6 @@
+//FeedbackForm.spec.js javascript based file
+//It will fetches from page-object folder
+//That is it takes class names from Feedback.js and navbar.js
 import { url } from "../../../config"
 import Feedback from "../page-objects/components/Feedback"
 import Navbar from "../page-objects/components/Navbar"
